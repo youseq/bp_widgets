885757c45a77f182a73fa22b305136ef7afb948c
daniel <daniel.holdbrook@youseq.com>
2022-03-10 14:25:22 +0000

    first commit

---
+++ test_widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello() {
+    return <h1>Hello World!</h1>;
+  }
+
+ReactDOM.render(<Hello />, document.getElementById('myplot'))
+
+
+
+
+
